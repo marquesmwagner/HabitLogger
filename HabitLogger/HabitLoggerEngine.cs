0ea2039e278332620a2f8369364da70b8cf8eeaf
marquesmwagner <marquesmwagner@gmail.com>
2022-12-12 17:51:45 -0300

    Create HabitLoggerEngine Class.

---
+++ HabitLogger/HabitLoggerEngine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitLogger
+{
+    internal class HabitLoggerEngine
+    {
+        static string connectionString = @"Data Source=habit-logger.db";
+        internal static void CreateDatabase()
+        {
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText =
+                    @"CREATE TABLE IF NOT EXISTS drinking_water (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Date TEXT,
+                        Quantity INTEGER
+                        )";
+
+                tableCmd.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
+    }
+}
--- HabitLogger/Program.cs
+++ HabitLogger/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Database.Connection();
+            HabitLoggerEngine.CreateDatabase();
             Menu.ShowMenu();
         }
     }
