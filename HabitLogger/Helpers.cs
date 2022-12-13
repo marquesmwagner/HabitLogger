48965605c2545a02a0600a8c0a4ac236d123615c
marquesmwagner <marquesmwagner@gmail.com>
2022-12-13 10:12:11 -0300

    Add GetDateInput method.

--- HabitLogger/Helpers.cs
+++ HabitLogger/Helpers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,20 @@ namespace HabitLogger
 {
     internal class Helpers
     {
+        internal static string GetDateInput()
+        {
+            Console.WriteLine("\nPlease insert the date: (dd-mm-yy). Type 0 to return to menu.\n");
+            string dateInput = Console.ReadLine().Trim();
+
+            if (dateInput == "0") Menu.ShowMenu();
+
+            while (!DateTime.TryParseExact(dateInput,"dd-MM-yy", new CultureInfo("en-US"), DateTimeStyles.None, out _))
+            {
+                Console.WriteLine("\nInvalid date. (Format:dd-mm-yy). Type 0 to return to menu or try again.");
+                dateInput = Console.ReadLine();
+            }
+
+            return dateInput;
+        }
     }
 }
