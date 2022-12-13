af92f94fc64b66fdda2c458c5f3382e25771c4db
marquesmwagner <marquesmwagner@gmail.com>
2022-12-13 11:19:07 -0300

    Add GetAllRecords method.

--- HabitLogger/HabitLoggerEngine.cs
+++ HabitLogger/HabitLoggerEngine.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HabitLogger.Models;
 
 namespace HabitLogger
 {
@@ -15,6 +17,7 @@ namespace HabitLogger
             using (var connection = new SQLiteConnection(connectionString))
             {
                 connection.Open();
+                
                 var tableCmd = connection.CreateCommand();
 
                 tableCmd.CommandText =
@@ -27,6 +30,51 @@ namespace HabitLogger
                 tableCmd.ExecuteNonQuery();
 
                 connection.Close();
+            }
+        }
+        internal static void GetAllRecords() 
+        {
+            Console.Clear();
+
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText =
+                    "SELECT * FROM drinking_water";
+
+                List<Models.HabbitLogger> tableData = new();
+
+                SQLiteDataReader reader= tableCmd.ExecuteReader();
+
+                if (reader.HasRows) 
+                {
+                    while (reader.Read()) 
+                    {
+                        tableData.Add(
+                            new Models.HabbitLogger
+                            {
+                                Id = reader.GetInt32(0),
+                                Date = DateTime.ParseExact(reader.GetString(1), "dd-MM-yy", new CultureInfo("en-US")),
+                                Quantity = reader.GetInt32(2)
+                            }); ;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found");
+                }
+
+                connection.Close();
+
+                Console.WriteLine("-----------------------------------\n");
+                foreach (var dw in tableData)
+                {
+                    Console.WriteLine($"{dw.Id} - {dw.Date.ToString("dd-MM-yy")} - Quantity: {dw.Quantity}");
+                }
+                Console.WriteLine("\n-----------------------------------\n");
             }
         }
         internal static void Insert()
