7ad1c06d9024e21e193ba3d9ab5cb036ae25702e
marquesmwagner <marquesmwagner@gmail.com>
2022-12-14 11:12:32 -0300

    Add Delete method.

--- HabitLogger/HabitLoggerEngine.cs
+++ HabitLogger/HabitLoggerEngine.cs
@@ -12,6 +12,7 @@ namespace HabitLogger
     internal class HabitLoggerEngine
     {
         static string connectionString = @"Data Source=habit-logger.db";
+        static bool listIsEmpty = true;
         internal static void CreateDatabase()
         {
             using (var connection = new SQLiteConnection(connectionString))
@@ -47,7 +48,7 @@ namespace HabitLogger
 
                 List<Models.HabbitLogger> tableData = new();
 
-                SQLiteDataReader reader= tableCmd.ExecuteReader();
+                SQLiteDataReader reader = tableCmd.ExecuteReader();
 
                 if (reader.HasRows) 
                 {
@@ -59,12 +60,14 @@ namespace HabitLogger
                                 Id = reader.GetInt32(0),
                                 Date = DateTime.ParseExact(reader.GetString(1), "dd-MM-yy", new CultureInfo("en-US")),
                                 Quantity = reader.GetInt32(2)
-                            }); ;
+                            });
                     }
+                    listIsEmpty = false;
                 }
                 else
                 {
                     Console.WriteLine("No rows found");
+                    listIsEmpty = true;
                 }
 
                 connection.Close();
@@ -96,6 +99,40 @@ namespace HabitLogger
                 connection.Close();
             }
         }
-        
+        internal static void Delete()
+        {
+            GetAllRecords();
+
+            if (listIsEmpty)
+            {
+                Console.WriteLine("\nThe list is empty. You can't delete any record.");
+                return;
+            }
+            
+            var recordId = Helpers.GetNumberInput("\nPlease type the Id of the record you want to exclude or type 0 to go back to menu.\n");
+
+            using (var connection = new SQLiteConnection(connectionString)) 
+            {
+                connection.Open();
+                
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText =
+                    $"DELETE FROM drinking_water WHERE Id = '{recordId}'";
+
+                var rowCount = tableCmd.ExecuteNonQuery();
+
+                if (rowCount == 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"\nRecord with Id {recordId} doesn't exist.");
+                } else
+                {
+                    Console.WriteLine($"\nRecord with Id {recordId} was excluded.\n");
+                }
+
+                connection.Close();
+            }    
+        }
     }
 }
